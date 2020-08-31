1695e17fd8f4bd308e39a1c06b1c80b43c3c1009
gethin <gethin.yan@gmail.com>
2020-08-31 17:05:35 +0800

    sqlDB

--- models/models.go
+++ models/models.go
@@ -38,9 +38,9 @@ func init() {
 	}
 	// SetMaxIdleConns 设置空闲连接池中连接的最大数量
 	sqlDB.SetMaxIdleConns(10)
-	// SetMaxOpenConns 设置打开数据库连接的最大数量。
+	// SetMaxOpenConns 设置打开数据库连接的最大数量
 	sqlDB.SetMaxOpenConns(100)
-	// SetConnMaxLifetime 设置了连接可复用的最大时间。
+	// SetConnMaxLifetime 设置了连接可复用的最大时间
 	sqlDB.SetConnMaxLifetime(time.Hour)
 
 }
